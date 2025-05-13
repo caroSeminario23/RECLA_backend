f764435854952ebaca23abac0b17d26fa2a6e45a
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2025-05-13 10:51:21 -0500

    Modelos de clases para login y registro

---
+++ models/racha.py
@@ -0,0 +1,30 @@
+from utils.db import db
+
+class Racha(db.Model):
+    __tablename__ = 'racha'
+
+    # Variables
+    id_racha = db.Column(
+        db.Integer, 
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+    )
+
+    id_pers = db.Column(
+        db.Integer,
+        db.ForeignKey('persona.id_pers'),
+        nullable=False
+    )
+
+    n_dias = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    # Constructor
+    def __init__(self,
+                id_pers,
+                n_dias): 
+        self.id_pers=id_pers
+        self.n_dias=n_dias
